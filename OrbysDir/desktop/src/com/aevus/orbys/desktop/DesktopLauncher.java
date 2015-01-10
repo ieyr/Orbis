0b8bc9760f30f3ee6e8fb736a740d32998c9e916
Rohan Iyer <rohan.iyer1999@gmail.com>
2015-01-10 15:48:50 -0800

    Project Creation

---
+++ OrbysDir/desktop/src/com/aevus/orbys/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aevus.orbys.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aevus.orbys.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
---
+++ OrbysDir/html/src/com/aevus/orbys/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.aevus.orbys.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.aevus.orbys.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyGdxGame();
+        }
+}
